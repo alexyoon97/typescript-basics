800c2405a11fb45c00d3d52cd0a1025ffbf5c75c
Alex <wonsangalex@gmail.com>
2021-06-01 16:33:06 -0700

    union/literal type

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + n1 + n2);
+//Union Function
+function combi(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
-        return n1 + n2;
+        result = input1.toString() + input2.toString();
     }
+    return result;
+    //   if(resultConversion === 'as-number'){
+    //       return +result;
+    //   }
+    //   else{
+    //       return result.toString()
+    //   }
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+var combiedAges = combi(30, 26, "as-number");
+var combiedStringAges = combi('30', '26', "as-number");
+var combinedNames = combi("Max", " Anna", "as-text");
+//Literal types
--- app.ts
+++ app.ts
@@ -1,16 +1,27 @@
 //Union Function
 
-function combi(input1: number | string, input2: number | string){
-    let result;
-    if( typeof input1 === 'number' && typeof input2 ==='number'){
-        result = input1 + input2
-    }
-    else{
-        result = input1.toString() + input2.toString()
-    }
-    return result
+function combi(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text' //literal type
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result
+//   if(resultConversion === 'as-number'){
+//       return +result;
+//   }
+//   else{
+//       return result.toString()
+//   }
 }
 
-const combiedAges = combi(30,26)
+const combiedAges = combi(30, 26, "as-number");
 
-const combinedNames = combi('Max', ' Anna')
+const combiedStringAges = combi('30', '26', "as-number");
+
+const combinedNames = combi("Max", " Anna", "as-text");
